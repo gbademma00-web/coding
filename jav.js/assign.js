2330ed38de10de0df34227fe5ee5e8dcaa30c0be
gbademma00-web <gbadeyanemmanuel00@gmail.com>
2026-09-25 02:29:15 +0100

    Updating console

--- jav.js/assign.js
+++ jav.js/assign.js
@@ -3,10 +3,12 @@
 const Name = "Gbadeyan Emmanuel"
 const Age = 2026-2008
 const Institution = "Lagos State University"
-const Level = 300
+let Level = 300
 const Academy = "TechUp Academy"
-const Program = "Web Development"
+let Program = "Web Development"
 const id = true
+const NextYear = Age + 1
+
 
 console.log("My Name is " + Name)
 console.log("I am " + Age + " years old")
@@ -14,5 +16,6 @@ console.log("I attend " + Institution)
 console.log("I am in " + Level + " level")
 console.log("I enrolled at " + Academy)
 console.log("I am learning " + Program)
-console.log(Age<18 == id)
-console.log(Age>=18 == id)
+console.log(Age<18 == id, "I am " + Age + " years old")
+console.log(Age>=18 == id, "I am " + Age + " years old")
+console.log("Next year, I will be " + NextYear + " years old")
